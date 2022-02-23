acab92761db8526db1cfd3efd18d5ea03dadab2e
hanokh <hanokh.aloni@gmail.com>
2022-02-23 13:57:33 +0200

    client to use weatherService instead of greeterService

--- weather.io.grpc.console/Program.cs
+++ weather.io.grpc.console/Program.cs
@@ -11,6 +11,26 @@ namespace weather.io.grpc.console
         {
             // The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Weather.WeatherClient(channel);
+            var reply = await client.GetWeatherAsync(
+                              new WeatherRequest { Name = "GreeterClient" });
+            Console.WriteLine("Weather: " + reply.Greet);
+            foreach (var forecast in reply.Items)
+            {
+                Console.WriteLine("On {0} it will be {1}C / {2}F - {3}"
+                    , forecast.Date
+                    , forecast.TemperatureC
+                    , forecast.TemperatureF
+                    , forecast.Summary);
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static async Task Greet(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
@@ -18,5 +38,6 @@ namespace weather.io.grpc.console
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
+
     }
 }
