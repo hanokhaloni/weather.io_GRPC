92573c998d2b920703704d5e81d4b70f557c95bd
hanokh <hanokh.aloni@gmail.com>
2022-02-23 16:12:55 +0200

    Deprecated config, using commandline params.

--- weather.io.grpc.console/Program.cs
+++ weather.io.grpc.console/Program.cs
@@ -3,39 +3,49 @@ using System.Threading.Tasks;
 using Grpc.Net.Client;
 
 using System.Configuration;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace weather.io.grpc.console
 {
+    [Command(Name = "The application name", Description = "The application description")]
+    [HelpOption("-?")]
     class Program
     {
-        static async Task Main(string[] args)
-        {
-            var serverBaseUrl = ConfigurationManager.AppSettings.Get("ServerBaseUrl");
+        [Option("-s|--server", Description = "Sets server base url")]
+        private string ServerBaseUrl { get; set; }
 
-            if (String.IsNullOrEmpty(serverBaseUrl))
+        [Option("-g|--greet", Description = "Calls additional greet request")]
+        private bool ShouldGreet { get; set; }
+
+        static void Main(string[] args)
+        {
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        private void OnExecute()
+        {
+            if (String.IsNullOrEmpty(ServerBaseUrl))
             {
                 throw new ArgumentException("Strange - no ServerBaseUrl found. Exiting...");
             }
 
-            if (args.Length == 1)
+            if (ShouldGreet)
             {
-                if (args[0].Contains("Greet", StringComparison.OrdinalIgnoreCase))
-                {
-                    await Greet(serverBaseUrl);
-                }
+                Greet(ServerBaseUrl);
             }
-            await GetWeatherForecast(serverBaseUrl);
+
+            GetWeatherForecast(ServerBaseUrl);
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
 
-        private static async Task GetWeatherForecast(string serverBaseUrl)
+        private void GetWeatherForecast(string serverBaseUrl)
         {
             // The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress(serverBaseUrl);
             var client = new Weather.WeatherClient(channel);
-            var reply = await client.GetWeatherAsync(new WeatherRequest { Name = "GreeterClient" });
+            var reply =  client.GetWeather(new WeatherRequest { Name = "GreeterClient" });
 
             Console.WriteLine("Weather: " + reply.Greet);
             foreach (var forecast in reply.Items)
@@ -45,12 +55,12 @@ namespace weather.io.grpc.console
             }
         }
 
-        static async Task Greet(string serverBaseUrl)
+        private void Greet(string serverBaseUrl)
         {
             // The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress(serverBaseUrl);
             var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(
+            var reply = client.SayHello(
                               new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
         }
